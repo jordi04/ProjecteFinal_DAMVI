7d62ef3f12f200f9bce155db7908fe0710fe6417
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2025-05-11 20:34:35 +0200

    Sons del bolet afegits (excepte death que no en tenim)

--- Assets/Scripts/Tools/MoveToCamera.cs
+++ Assets/Scripts/Tools/MoveToCamera.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -27,3 +28,4 @@ public static class MoveSelectedToSceneCameraShortcut
         selectedObject.transform.position = sceneCam.transform.position;
         }
 }
+#endif

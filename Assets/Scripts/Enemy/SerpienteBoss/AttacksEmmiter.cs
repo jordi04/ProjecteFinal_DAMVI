91bc6b35e62e8c471ff20ad80b79acdb02ff79ed
jordi04 <jordipauroca@gmail.com>
2025-05-12 04:45:46 +0200

    Update AttacksEmmitters per a cinematica Marc

--- Assets/Scripts/Enemy/SerpienteBoss/AttacksEmmiter.cs
+++ Assets/Scripts/Enemy/SerpienteBoss/AttacksEmmiter.cs
@@ -3,11 +3,14 @@ using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.Playables;
 
 public class AttacksEmmiter : MonoBehaviour
 {
     [SerializeField] SerpienteBoss snake;
-    
+    [SerializeField] PlayableDirector playableDirector;
+    [SerializeField] GameObject hud;
     public void closeAttackEmmiter()
     {
         ManaSystem.instance.TakeDamage(snake.dañoMordida);
@@ -36,5 +39,31 @@ public class AttacksEmmiter : MonoBehaviour
         instance.set3DAttributes(RuntimeUtils.To3DAttributes(snake.snakePosition));
         instance.start();
         instance.release();
+    }
+
+    public void startDeathCinematic() //Comenca animació/cinematica
+    {
+        hud.SetActive(false);
+        // Disable player input during cinematic
+        if (UserInput.instance != null)
+        {
+            UserInput.instance.switchActionMap(UserInput.ActionMap.InCinematic);
+        }
+
+        // Optional: Start a coroutine to restore player input after a delay
+        StartCoroutine(RestorePlayerInputAfterDelay((float)playableDirector.duration)); //playableDirector.duration
+    }
+
+    private IEnumerator RestorePlayerInputAfterDelay(float delay) //Quan acaba cinemàtica
+    {
+        yield return new WaitForSeconds(delay);
+
+        hud.SetActive(true);
+
+        if (UserInput.instance != null)
+        {
+            UserInput.instance.switchActionMap(UserInput.ActionMap.InGame);
+        }
+    }
     }
 }
